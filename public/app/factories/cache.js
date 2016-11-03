3762d6d6d43bc154c228bee093f1f33d733405f6
uxin603s <uxin603s@gmail.com>
2016-11-03 11:18:41 +0800

    新增清除快取機制

--- public/app/factories/cache.js
+++ public/app/factories/cache.js
@@ -1,13 +1,20 @@
 angular.module('app').factory('cache',['$rootScope',function($rootScope){
-	var cache={};
+	var cache={
+		data:{}
+	};
 	localforage.getItem(location.pathname+"cache").then(function(data){
-		for(var i in data){
-			cache[i]=data[i];
+		if(data.cache_time && (Math.floor(Date.now()/1000) - 5) > data.cache_time ){
+			console.log("太久沒來了清除快取")
+		}else{
+			for(var i in data.data){
+				cache.data[i]=data.data[i];
+			}
+			$rootScope.$apply();
 		}
-		$rootScope.$apply();
 		setInterval(function(){
+			cache.cache_time=Math.floor(Date.now()/1000);
 			localforage.setItem(location.pathname+"cache",angular.copy(cache));
 		},500)
 	});
-	return cache;
+	return cache.data;
 }])
