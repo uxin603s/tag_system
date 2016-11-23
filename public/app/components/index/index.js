1c040654bf6a190b629d9faaffeb20ae4c83c2e6
uxin603s <uxin603s@gmail.com>
2016-11-23 12:03:29 +0800

    搜尋與新增模式

--- public/app/components/index/index.js
+++ public/app/components/index/index.js
@@ -5,32 +5,61 @@ angular.module('app').component("index",{
 		$scope.cache=cache;
 		
 		postMessageHelper.receive("tagSystem",function(res){
-			if(res.name=="getTag"){
-				var getTag_timer=setInterval(function(){
-					if(cache.webList && cache.webList.list[cache.webList.select] && cache.levelList){
-						clearTimeout(getTag_timer);
-						var wid=cache.webList.list[cache.webList.select].id
-						var level_id=cache.levelList[cache.levelList.length-1].id;
+			var getTag_timer=setInterval(function(){
+				if(cache.webList && cache.webList.list[cache.webList.select] && cache.levelList){
+					clearTimeout(getTag_timer);
+					var wid=cache.webList.list[cache.webList.select].id
+					var level_id=cache.levelList[cache.levelList.length-1].id;
+					if(res.name=="getTag"){
 						idRelation.get(res.value,wid,level_id)
 						.then(function(res){
 							var list=angular.copy(res);
+							var result={};
 							for(var i in list){
+								result[i]=[];
 								for(var j in list[i]){
-									list[i][j].name=cache.tagName[list[i][j].id]
-									
+									result[i].push(cache.tagName[list[i][j].id]);
 								}
 							}
-							postMessageHelper.send("tagSystem",{name:"getTag",value:list})
+							postMessageHelper.send("tagSystem",{name:"getTag",value:result})
 						});
-						
 					}
-				},0)
-			}
+					else if(res.name=="addTag"){
+						idRelation.add(res.value.name,res.value.id,level_id,wid);
+					}
+					else if(res.name=="delTag"){
+						// console.log(res.value)
+						idRelation.del(res.value.index,res.value.id);
+					}
+				}
+			},0)
 			$scope.$apply();
 		})
-		
-		// $scope.$watch("cache.webList.list",watch_receive)
-		// $scope.$watch("cache.levelList",watch_receive)
+		var watch_selectList;
+		$scope.$watch("cache.selectList",function(selectList){
+			return
+			if(!selectList)return;
+			clearTimeout(watch_selectList)
+			watch_selectList=setTimeout(function(){
+				// console.log(selectList)
+				var selectTag;
+				for(var i in selectList){
+					var data=selectList[i][selectList[i].length-1];
+					
+					if(data.select){
+						selectTag=cache.tagName[data.select];
+						delete data.select
+						break;
+					}
+				}
+				if(selectTag){
+					postMessageHelper.send("tagSystem",{name:"selectTag",value:selectTag})
+				}
+				
+			},0)
+			
+			
+		},1)
 	
 		$scope.$watch("cache.tag_search.result",function(value){
 			if(!value)return;
