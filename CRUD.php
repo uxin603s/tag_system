a73600d19c3f30b611b111e1fdcf4109dd31c4fa
uxin603s <uxin603s@gmail.com>
2016-12-10 08:00:37 +0800

    CRUD加入可自訂select

--- CRUD.php
+++ CRUD.php
@@ -7,8 +7,14 @@ trait CRUD{
 					unset($list[$field]);
 				}
 			}else{
-				if(!in_array($field,self::$filter_field_arr)){
-					unset($list[$field]);
+				if(is_numeric($field)){
+					if(!in_array($value,self::$filter_field_arr)){
+						unset($list[$field]);
+					}
+				}else{
+					if(!in_array($field,self::$filter_field_arr)){
+						unset($list[$field]);
+					}
 				}
 			}
 		}
@@ -16,13 +22,21 @@ trait CRUD{
 	}
 	public static function getList($arg){
 		$bind_data=[];
+		$select_str="*";
+		if($arg['where_list']){
+			$arg['select_list']=self::filter_field($arg['select_list']);
+			if(is_array($arg['select_list']) && count($arg['select_list'])){
+				$select_str=implode(",",$arg['select_list']);
+			}
+		}
+		
 		$where_str=MysqlCompact::where(self::filter_field($arg['where_list']),$bind_data);
 		$orderBy_str=MysqlCompact::order(self::filter_field($arg['order_list']));
 		$group_str=MysqlCompact::group(self::filter_field($arg['group_list']),$bind_data);
 		
 		$limit_str=MysqlCompact::limit($arg['limit']);
 		
-		$sql="select * from ".self::$table." 
+		$sql="select {$select_str} from ".self::$table." 
 		{$where_str} {$orderBy_str} {$group_str} {$limit_str}";
 		
 		if($tmp=DB::select($sql,$bind_data)){
@@ -33,11 +47,13 @@ trait CRUD{
 		}else{
 			$status=false;
 		}
-		$count_sql="select count(*) count from ".self::$table." 
-		{$where_str} {$orderBy_str} {$group_str} ";
-		if($tmp=DB::select($count_sql,$bind_data)){
-			$total_count=$tmp[0]['count'];
-			$total_page=ceil($total_count/$arg['limit']['count']);
+		if($limit_str){
+			$count_sql="select count(*) count from ".self::$table." 
+			{$where_str} {$orderBy_str} {$group_str} ";
+			if($tmp=DB::select($count_sql,$bind_data)){
+				$total_count=$tmp[0]['count'];
+				$total_page=ceil($total_count/$arg['limit']['count']);
+			}
 		}
 		return compact(['status','list','sql','bind_data','total_page','total_count']);
 	}
