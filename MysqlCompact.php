cc9af79de8140a2bdf92171b10d1bd7901871375
uxin603s <uxin603s@gmail.com>
2016-12-10 10:34:00 +0800

    增加空白

--- MysqlCompact.php
+++ MysqlCompact.php
@@ -15,41 +15,44 @@ class MysqlCompact{
 		"like",
 		"not like",
 	];
-	public static function where($where_list=[],&$bind_data){
-		$query_str="where 1=1 ";
-		
-		$used_count=array_count_values(array_column($where_list,'field'));
-		$or_array=[];
-		$and_array=[];
-		foreach($used_count as $field=>$count){
-			if($count>1){
-				$or_array[]=$field;
-			}else{
-				$and_array[]=$field;
-			}
-		}
-		$or_where=[];
-		foreach($where_list as $item){
-			$field=$item['field'];
-			$type=$item['type'];
-			$value=$item['value'];
-			$symbol=self::$type_list[$type];
-			//欄位白名單
-			if($symbol){
-				if(in_array($field,$and_array)){
-					$query_str.=" && {$field} {$symbol} ?";
-					$bind_data[]=$value;
-				}else if(in_array($field,$or_array)){
-					$or_where[$field]['query'][]=" {$field} {$symbol} ? ";
-					$or_where[$field]['value'][]=$value;
+	public static function where($list,&$bind_data){
+		$query_str='';
+		if(is_array($list) && count($list)){
+			$query_str.=" ";
+			$query_str.="where 1=1 ";
+			$used_count=array_count_values(array_column($list,'field'));
+			$or_array=[];
+			$and_array=[];
+			foreach($used_count as $field=>$count){
+				if($count>1){
+					$or_array[]=$field;
+				}else{
+					$and_array[]=$field;
 				}
 			}
+			$or_where=[];
+			foreach($list as $item){
+				$field=$item['field'];
+				$type=$item['type'];
+				$value=$item['value'];
+				$symbol=self::$type_list[$type];
+				//欄位白名單
+				if($symbol){
+					if(in_array($field,$and_array)){
+						$query_str.=" && {$field} {$symbol} ?";
+						$bind_data[]=$value;
+					}else if(in_array($field,$or_array)){
+						$or_where[$field]['query'][]=" {$field} {$symbol} ? ";
+						$or_where[$field]['value'][]=$value;
+					}
+				}
+			}
+			foreach($or_where as $field=>$item){
+				$query_str.=" && (".implode(" || ",$item['query']).")";
+				$bind_data=array_merge($bind_data,$item['value']);
+			}
+			$query_str.=" ";
 		}
-		foreach($or_where as $field=>$item){
-			$query_str.=" && (".implode(" || ",$item['query']).")";
-			$bind_data=array_merge($bind_data,$item['value']);
-		}
-		
 		return $query_str;
 	}
 	public static $sort_list=[
@@ -59,7 +62,8 @@ class MysqlCompact{
 	public static function order($list){
 		$query_str='';
 		if(is_array($list) && count($list)){
-			$query_str="order by ";
+			$query_str.=" ";
+			$query_str.="order by ";
 			$array=[];
 			foreach($list as $item){
 				$field=$item['field'];
@@ -69,38 +73,47 @@ class MysqlCompact{
 				}
 			}
 			$query_str.=implode(",",$array);
+			$query_str.=" ";
 		}
 		return $query_str;
 	}
 	public static function group($list){
 		$query_str='';
 		if(is_array($list) && count($list)){
+			$query_str.=" ";
 			$query_str="group by ".implode(",",$list);
+			$query_str.=" ";
 		}
 		return $query_str;
 	}
 	public static function have($list,&$bind_data){
 		$query_str='';
-		foreach($list as $item){
-			if($item['type']==0){
-				if(is_array($item['value'])){
-					$query_str="having count({$item['field']})  >=  ? ";
-					$bind_data[]=array_shift($item['value']);
-					foreach($item['value'] as $value){
-						$query_str.=" && max( CASE `{$item['field']}`  WHEN ? THEN 1 ELSE 0 END ) = 1";
-						$bind_data[]=$value;
+		if(is_array($list) && count($list)){
+			$query_str.=" ";
+			foreach($list as $item){
+				if($item['type']==0){
+					if(is_array($item['value'])){
+						$query_str.="having count({$item['field']})  >=  ? ";
+						$bind_data[]=array_shift($item['value']);
+						foreach($item['value'] as $value){
+							$query_str.=" && max( CASE `{$item['field']}`  WHEN ? THEN 1 ELSE 0 END ) = 1";
+							$bind_data[]=$value;
+						}
 					}
 				}
 			}
+			$query_str.=" ";
 		}
 		return $query_str;
 	}
 	public static function limit($data=[]){
 		$query_str='';
 		if(count($data) && is_numeric($data['page']) && is_numeric($data['count'])){
+			$query_str.=" ";
 			$start=$data['page']*$data['count'];
 			$count=$data['count'];
-			$query_str.=" limit {$start},{$count}";
+			$query_str.="limit {$start},{$count}";
+			$query_str.=" ";
 		}
 		return $query_str;
 	}
