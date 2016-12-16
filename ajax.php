ebf8508752352906a98689c906fdea03f9b5d1cc
uxin603s <uxin603s@gmail.com>
2016-12-16 16:45:06 +0800

    update

--- ajax.php
+++ ajax.php
@@ -23,14 +23,5 @@ DB::query("SET NAMES utf8");
 session_start();
 $_SESSION['id']=0;
 session_write_close();
-if(isset($argv[1])){
-	array_shift($argv);
-	$_REQUEST['func_name']=array_shift($argv);
-	$_REQUEST['arg']=$argv;
-}
-if(isset($_REQUEST['func_name'])){
-	$func_name=$_REQUEST['func_name'];
-	$arg=empty($_REQUEST['arg'])?[]:$_REQUEST['arg'];
-	echo @json_encode(call_user_func($func_name,$arg),JSON_NUMERIC_CHECK);
-}
+include_once __DIR__."/github/MysqlCompact/API.php";
 DB::$connect=null;
