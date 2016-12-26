33c78ee9fe287a80fc23c7fc0b305a53e8c12906
uxin603s <uxin603s@gmail.com>
2016-12-26 14:48:04 +0800

    update

--- ajax.php
+++ ajax.php
@@ -1,24 +1,9 @@
 <?php
 
-include_once __DIR__."/github/DB/DB.php";
-include_once __DIR__."/github/MysqlCompact/MysqlCompact.php";
-include_once __DIR__."/github/MysqlCompact/CRUD.php";
-
-include_once __DIR__."/WebList.php";
-include_once __DIR__."/TagType.php";
-include_once __DIR__."/WebTagType.php";
-include_once __DIR__."/TagName.php";
-
-include_once __DIR__."/TagRelation.php";
-
-include_once __DIR__."/TagLevel.php";
-
-include_once __DIR__."/WebRelation.php";
+include_once __DIR__."/include.php";
 
 
-$DBConfig=json_decode(file_get_contents(__DIR__."/config/DB.json"),1);
-DB::$config=$DBConfig;
-DB::query("SET NAMES utf8");
+
 
 session_start();
 $_SESSION['id']=0;
