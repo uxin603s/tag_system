f47ad9731f84250ff572edd1d1033f79c0cb6f16
uxin603s <uxin603s@gmail.com>
2016-12-28 17:42:04 +0800

    update

--- WebRelation.php
+++ WebRelation.php
@@ -9,13 +9,22 @@ class WebRelation{
 		$WebRelationR=[];
 		if($tmp['status']){
 			foreach($tmp['list'] as $item){
-				$WebRelation[$item['wid']][$item['source_id']][]=$item['tid'];
-				$WebRelationR[$item['wid']][$item['tid']][]=$item['source_id'];
+				$WebRelation[$item['wid']][$item['source_id']][$item['tid']]=$item['tid'];
+				$WebRelationR[$item['wid']][$item['tid']][$item['source_id']]=$item['source_id'];
 			}
 		}
-		foreach($WebRelation as $wid=>$item){
-			Cache::group_save("WebRelation".$wid,$WebRelation[$wid]);
-			Cache::group_save("WebRelationR".$wid,$WebRelationR[$wid]);
+		
+		foreach($WebRelation as $wid=>$source_id_arr){
+			foreach($source_id_arr as $source_id=>$value){
+				$key="WebRelation.wid.{$wid}.source_id.{$source_id}";
+				Cache::group_save($key,$value);
+			}			
+		}
+		foreach($WebRelationR as $wid=>$tid_id_arr){
+			foreach($tid_id_arr as $tid=>$value){
+				$key="WebRelationR.wid.{$wid}.tid.{$tid}";
+				Cache::group_save($key,$value);
+			}
 		}
 	}
 	
--- flush_cache.php
+++ flush_cache.php
@@ -1,13 +1,13 @@
 <?php
 include_once __DIR__."/include.php";
 
-// TagLevel::flushCache();
-// TagName::flushCache();
-// TagRelation::flushCache();
-// TagType::flushCache();
-// WebTagType::flushCache();
-// WebRelation::flushCache();
-var_dump(Cache::group_get_all("WebRelation1"));
-var_dump(Cache::group_get_all("WebRelationR1"));
+TagLevel::flushCache();
+TagName::flushCache();
+TagRelation::flushCache();
+TagType::flushCache();
+WebTagType::flushCache();
+WebRelation::flushCache();
+// var_dump(Cache::group_get_all("WebRelation1"));
+// var_dump(Cache::group_get_all("WebRelation.wid.1.source_id.477"));
 // var_dump(Cache::get_one("TagRelation",1));
 exit;
